cf45909ef922b7fa45086be692b1d52cdec836fc
Jaden Baptista <jaden@baptista.dev>
2021-12-05 00:39:25 -0500

    bug fix

--- functions/language.js
+++ functions/language.js
@@ -2,96 +2,102 @@ import crypto from "crypto";
 import { v4 as uuidv4 } from 'uuid';
 import fetch from 'node-fetch';
 
+const getLanguage = async () => {
+	const {
+		name,
+		password
+	} = JSON.parse(event.queryStringParameters);
+
+	const response = await fetch(
+		"https://graphql.us.fauna.com/graphql",
+		{
+			headers: {
+				"Authorization": `Basic ${process.env.FAUNA_KEY}`
+			},
+			method: "POST",
+			body: JSON.stringify({
+				query: `
+					query {
+						languagesByCreds (
+							name: "${name}",
+							password: "${crypto.createHash("sha256").update(password).digest("hex")}"
+						) {
+							data {
+								id
+								name
+								creatorName
+								creatorEmail
+								notes
+								sentenceStructure
+								syllableStructures
+							}
+						}
+					}
+				`
+			})
+		}
+	);
+
+	return await response.json();
+};
+
+const createLanguage = async () => {
+	const id = uuidv4();
+	const {
+		name,
+		password,
+		creatorName,
+		creatorEmail,
+		notes,
+		sentenceStructure,
+		syllableStructures
+	} = JSON.parse(event.body);
+
+	const response = await fetch(
+		"https://graphql.us.fauna.com/graphql",
+		{
+			headers: {
+				"Authorization": `Basic ${process.env.FAUNA_KEY}`
+			},
+			method: "POST",
+			body: JSON.stringify({
+				query: `
+					mutation CreateLanguage {
+						createLanguage(
+							data: {
+								id: "${id}",
+								name: "${name}",
+								password: "${crypto.createHash("sha256").update(password).digest("hex")}",
+								creatorName: "${creatorName}",
+								creatorEmail: "${creatorEmail}",
+								notes: ${JSON.stringify(notes)}
+								sentenceStructure: "${sentenceStructure}"
+								syllableStructures: ${JSON.stringify(syllableStructures)}
+							}
+						) {
+							id
+						}
+					}
+				`
+			})
+		}
+	);
+	return await response.json();
+};
+
 exports.handler = async function (event, context) {
 	switch (event.httpMethod) {
 		case "POST":
-			const id = uuidv4();
-			const {
-				name,
-				password,
-				creatorName,
-				creatorEmail,
-				notes,
-				sentenceStructure,
-				syllableStructures
-			} = JSON.parse(event.body);
-
-			const response = await fetch(
-				"https://graphql.us.fauna.com/graphql",
-				{
-					headers: {
-						"Authorization": `Basic ${process.env.FAUNA_KEY}`
-					},
-					method: "POST",
-					body: JSON.stringify({
-						query: `
-							mutation CreateLanguage {
-								createLanguage(
-									data: {
-										id: "${id}",
-										name: "${name}",
-										password: "${crypto.createHash("sha256").update(password).digest("hex")}",
-										creatorName: "${creatorName}",
-										creatorEmail: "${creatorEmail}",
-										notes: ${JSON.stringify(notes)}
-										sentenceStructure: "${sentenceStructure}"
-										syllableStructures: ${JSON.stringify(syllableStructures)}
-									}
-								) {
-									id
-								}
-							}
-						`
-					})
-				}
-			);
-			const results = await response.json();
-
-			return {
-				statusCode: 200,
-				body: JSON.stringify(results)
-			};
+			let results = await createLanguage();
+			break;
 
 		case "GET":
-			const {
-				name,
-				password
-			} = JSON.parse(event.queryStringParameters);
+			let results = await getLanguage();
+			break;
 
-			const response = await fetch(
-				"https://graphql.us.fauna.com/graphql",
-				{
-					headers: {
-						"Authorization": `Basic ${process.env.FAUNA_KEY}`
-					},
-					method: "POST",
-					body: JSON.stringify({
-						query: `
-							query {
-								languagesByCreds (
-									name: "${name}",
-									password: "${crypto.createHash("sha256").update(password).digest("hex")}"
-								) {
-									data {
-  										id
-										name
-										creatorName
-										creatorEmail
-										notes
-										sentenceStructure
-										syllableStructures
-									}
-								}
-							}
-						`
-					})
-				}
-			);
-			const results = await response.json();
-
-			return {
-				statusCode: 200,
-				body: JSON.stringify(results)
-			};
+		return {
+			statusCode: 200,
+			body: JSON.stringify(results)
+		};
 	}
 }
