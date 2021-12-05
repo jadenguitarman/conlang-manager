d86b9552a5ecac806f7b0c25809f7d838e7f71a7
Jaden Baptista <jaden@baptista.dev>
2021-12-05 00:22:30 -0500

    add get

---
+++ functions/language.js
@@ -0,0 +1,97 @@
+import crypto from "crypto";
+import { v4 as uuidv4 } from 'uuid';
+import fetch from 'node-fetch';
+
+exports.handler = async function (event, context) {
+	switch (event.httpMethod) {
+		case "POST":
+			const id = uuidv4();
+			const {
+				name,
+				password,
+				creatorName,
+				creatorEmail,
+				notes,
+				sentenceStructure,
+				syllableStructures
+			} = JSON.parse(event.body);
+
+			const response = await fetch(
+				"https://graphql.us.fauna.com/graphql",
+				{
+					headers: {
+						"Authorization": `Basic ${process.env.FAUNA_KEY}`
+					},
+					method: "POST",
+					body: JSON.stringify({
+						query: `
+							mutation CreateLanguage {
+								createLanguage(
+									data: {
+										id: "${id}",
+										name: "${name}",
+										password: "${crypto.createHash("sha256").update(password).digest("hex")}",
+										creatorName: "${creatorName}",
+										creatorEmail: "${creatorEmail}",
+										notes: ${JSON.stringify(notes)}
+										sentenceStructure: "${sentenceStructure}"
+										syllableStructures: ${JSON.stringify(syllableStructures)}
+									}
+								) {
+									id
+								}
+							}
+						`
+					})
+				}
+			);
+			const results = await response.json();
+
+			return {
+				statusCode: 200,
+				body: JSON.stringify(results)
+			};
+
+		case "GET":
+			const {
+				name,
+				password
+			} = JSON.parse(event.queryStringParameters);
+
+			const response = await fetch(
+				"https://graphql.us.fauna.com/graphql",
+				{
+					headers: {
+						"Authorization": `Basic ${process.env.FAUNA_KEY}`
+					},
+					method: "POST",
+					body: JSON.stringify({
+						query: `
+							query {
+								languagesByCreds (
+									name: "${name}",
+									password: "${crypto.createHash("sha256").update(password).digest("hex")}"
+								) {
+									data {
+  										id
+										name
+										creatorName
+										creatorEmail
+										notes
+										sentenceStructure
+										syllableStructures
+									}
+								}
+							}
+						`
+					})
+				}
+			);
+			const results = await response.json();
+
+			return {
+				statusCode: 200,
+				body: JSON.stringify(results)
+			};
+	}
+}
